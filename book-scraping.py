a04e8a174b87da4d8dcbe8333602c9537c05b338
Matias Di Lella <matias.adilella@gmail.com>
2025-08-20 21:46:11 -0300

    Primera extracción del html

---
+++ book-scraping.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import requests as req
+from bs4 import BeautifulSoup
+import time
+import json
+
+URL_BASE = 'https://es.wikipedia.org/wiki/Estado_miembro_de_las_Naciones_Unidas'
+
+# 1. Obtención del HTML
+info_obtenida = req.get(URL_BASE)
+
+html_obtenido = info_obtenida.text
+
+# 2. Parseo del HTML
+soup = BeautifulSoup(html_obtenido, "html.parser")
+
+# Obtenemos una sección del HTML, filtrando por class
+h2_all = soup.find_all('div', class_="mw-heading mw-heading2")
+print(h2_all)
+
+# Iterar sobre el objeto
+for seccion in h2_all:
+  print('h2: ', seccion.text)
+
+# Se quitan los espacios innecesarios
+for seccion in h2_all:
+  print(seccion.get_text(strip=True))
+
+print('###################################')  
+
+# Todas las etiquetas que tengan el atributo "src"
+src_todos = soup.find_all(src=True)
+for elemento in src_todos:
+  if elemento['src'].endswith(".jpg"):
+    print(elemento)
+
+# Pausa de 2 segundos entre cada solicitud para evitar sobre cargar el servidor
+    time.sleep(2)
+
